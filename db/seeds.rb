63578408b54640d5ff559ab3ec32e47da194fc8c
Miyabimaru <gregoire.dubelloy@epitech.eu>
2018-07-05 16:24:44 +0200

    Client search index

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,11 @@
 
 #Users
 User.create(email: "gregoire.dubelloy@gmail.com", password: "password", password_confirmation: "password")
+
+#RechercheClient
+RechercheClient.create(ct_num: "012345", ct_type: 0, ct_classement: "Gueny Bernard", ct_adresse: "8 impasse beguin",
+                       ct_codepostal: "72000", ct_ville: "Le Mans", ct_email: "801215259@cwill.fr",
+                       ct_telephone: "0243248856")
+RechercheClient.create(ct_num: "012346", ct_type: 1, ct_classement: "Drouet Pamela", ct_adresse: "13 rue des ecoles",
+                       ct_codepostal: "17330", ct_ville: "doeuil sur le mignon", ct_email: "pameloche@hotmail.fr",
+                       ct_telephone: "+33 6 50 75 51 25")
